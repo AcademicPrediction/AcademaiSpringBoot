1b726ddb89c63b232f194136bb6946d4bc1ff5d9
ThePain99 <u201823838@upc.edu.pe>
2023-08-01 18:08:52 -0500

    First commit

---
+++ src/main/java/com/tp2/academaispringboot/AcademaiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tp2.academaispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademaiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademaiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tp2/academaispringboot/AcademaiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp2.academaispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademaiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
